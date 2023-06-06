d9ed1ae1592586260faedcda182b212136c06aff
Grosbin <grosbin.rivera@unah.hn>
2023-06-06 17:17:27 -0600

    random pokemon

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Quien es este pokemon?</h1>
+  <pokemon-picture :pokemonId="4" :showPokemon="true" />
+  <pokemon-option />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOption from "@/components/PokemonOption.vue";
+export default {
+  components: { PokemonPicture, PokemonOption },
+};
+</script>
+
+<style></style>
